124ce9f9a0533c60796105053f03018ecc4fee55
Lucas Maglio Chiabai <lucasmaglionovo@gmail.com>
2026-05-27 18:17:56 -0300

    Add Java program to calculate area of a circle

---
+++ 1002.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+
+        double R;
+        double A;
+
+        R = input.nextDouble();
+
+        A = 3.14159 * Math.pow(R, 2);
+
+        System.out.printf("A=%.4f\n", A);
+
+        input.close();
+
+    }
+}
